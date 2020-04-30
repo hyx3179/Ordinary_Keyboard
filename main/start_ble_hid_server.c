5ef8d450ac1e23a712c52c025f4c2b5589a102f0
hyx3179 <hyx3179@gmail.com>
2020-04-30 17:53:21 +0800

    蓝牙连接部分分出main

---
+++ main/start_ble_hid_server.c
@@ -0,0 +1,142 @@
+﻿/* This example code is in the Public Domain (or CC0 licensed, at your option.)
+   Unless required by applicable law or agreed to in writing, this software is
+   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <string.h>
+#include "esp_log.h"
+#include "esp_bt.h"
+
+#include "esp_hidd_prf_api.h"
+#include "esp_bt_main.h"
+#include "hid_dev.h"
+
+#define HID_TAG               "HID"
+#define HIDD_DEVICE_NAME      "OK"
+
+uint16_t hid_conn_id = 0;
+bool sec_conn = false;
+
+static uint8_t hidd_service_uuid128[] = {
+    /* LSB <--------------------------------------------------------------------------------> MSB */
+    //first uuid, 16bit, [12],[13] is the value
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0x12, 0x18, 0x00, 0x00,
+};
+
+static esp_ble_adv_data_t hidd_adv_data = {
+    .set_scan_rsp = false,
+    .include_name = true,
+    .include_txpower = true,
+    .min_interval = 0x0006, //slave connection min interval, Time = min_interval * 1.25 msec
+    .max_interval = 0x0010, //slave connection max interval, Time = max_interval * 1.25 msec
+    .appearance = 0x03c0,       //HID Generic
+    .manufacturer_len = 0,
+    .p_manufacturer_data = NULL,
+    .service_data_len = 0,
+    .p_service_data = NULL,
+    .service_uuid_len = sizeof(hidd_service_uuid128),
+    .p_service_uuid = hidd_service_uuid128,
+    .flag = 0x6,
+};
+
+static esp_ble_adv_params_t hidd_adv_params = {
+    .adv_int_min = 0x20,
+    .adv_int_max = 0x30,
+    .adv_type = ADV_TYPE_IND,
+    .own_addr_type = BLE_ADDR_TYPE_PUBLIC,
+    .channel_map = ADV_CHNL_ALL,
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+};
+
+
+static void hidd_event_callback(esp_hidd_cb_event_t event, esp_hidd_cb_param_t *param)
+{
+    switch (event) {
+        case ESP_HIDD_EVENT_REG_FINISH:
+            if (param->init_finish.state == ESP_HIDD_INIT_OK) {
+                esp_ble_gap_set_device_name(HIDD_DEVICE_NAME);
+                esp_ble_gap_config_adv_data(&hidd_adv_data);
+            }
+            break;
+        case ESP_BAT_EVENT_REG:
+            break;
+        case ESP_HIDD_EVENT_DEINIT_FINISH:
+            break;
+        case ESP_HIDD_EVENT_BLE_CONNECT:
+            ESP_LOGI(HID_TAG, "ESP_HIDD_EVENT_BLE_CONNECT");
+            hid_conn_id = param->connect.conn_id;
+            break;
+        case ESP_HIDD_EVENT_BLE_DISCONNECT:
+            sec_conn = false;
+            ESP_LOGI(HID_TAG, "ESP_HIDD_EVENT_BLE_DISCONNECT");
+            esp_ble_gap_start_advertising(&hidd_adv_params);
+            break;
+        case ESP_HIDD_EVENT_BLE_VENDOR_REPORT_WRITE_EVT:
+            ESP_LOGI(HID_TAG, "%s, ESP_HIDD_EVENT_BLE_VENDOR_REPORT_WRITE_EVT", __func__);
+            ESP_LOG_BUFFER_HEX(HID_TAG, param->vendor_write.data, param->vendor_write.length);
+        default:
+            break;
+    }
+    return;
+}
+
+static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
+{
+    switch (event) {
+        case ESP_GAP_BLE_ADV_DATA_SET_COMPLETE_EVT:
+            esp_ble_gap_start_advertising(&hidd_adv_params);
+            break;
+        case ESP_GAP_BLE_SEC_REQ_EVT:
+            for (int i = 0; i < ESP_BD_ADDR_LEN; i++) {
+                ESP_LOGD(HID_TAG, "%x:", param->ble_security.ble_req.bd_addr[i]);
+            }
+            esp_ble_gap_security_rsp(param->ble_security.ble_req.bd_addr, true);
+            break;
+        case ESP_GAP_BLE_AUTH_CMPL_EVT:
+            sec_conn = true;
+            esp_bd_addr_t bd_addr;
+            memcpy(bd_addr, param->ble_security.auth_cmpl.bd_addr, sizeof(esp_bd_addr_t));
+            ESP_LOGI(HID_TAG, "remote BD_ADDR: %08x%04x", (bd_addr[0] << 24) + (bd_addr[1] << 16) + (bd_addr[2] << 8) + bd_addr[3], (bd_addr[4] << 8) + bd_addr[5]);
+            ESP_LOGI(HID_TAG, "address type = %d", param->ble_security.auth_cmpl.addr_type);
+            ESP_LOGI(HID_TAG, "pair status = %s", param->ble_security.auth_cmpl.success ? "success" : "fail");
+            if (!param->ble_security.auth_cmpl.success) {
+                ESP_LOGE(HID_TAG, "fail reason = 0x%x", param->ble_security.auth_cmpl.fail_reason);
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+esp_err_t start_ble_hid_server()
+{
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+
+    /* 设置安全性 iocap & auth_req & key size & init key response key 堆栈参数*/
+    esp_ble_auth_req_t auth_req = ESP_LE_AUTH_BOND;     //认证后与对端设备绑定
+    esp_ble_io_cap_t iocap = ESP_IO_CAP_NONE;           //将IO能力设置为无输出无输入
+    uint8_t key_size = 16;      //密钥大小应为7〜16个字节
+    uint8_t init_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
+    uint8_t rsp_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
+
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
+    ESP_ERROR_CHECK(esp_bt_controller_init(&bt_cfg));
+    ESP_ERROR_CHECK(esp_bt_controller_enable(ESP_BT_MODE_BLE));
+    ESP_ERROR_CHECK(esp_bluedroid_init());
+    ESP_ERROR_CHECK(esp_bluedroid_enable());
+    ESP_ERROR_CHECK(esp_hidd_profile_init());
+
+    // 将回调函数注册到gap模块
+    ESP_ERROR_CHECK(esp_ble_gap_register_callback(gap_event_handler));
+    ESP_ERROR_CHECK(esp_hidd_register_callbacks(hidd_event_callback));
+
+    ESP_ERROR_CHECK(esp_ble_gap_set_security_param(ESP_BLE_SM_AUTHEN_REQ_MODE, &auth_req, sizeof(uint8_t)));
+    ESP_ERROR_CHECK(esp_ble_gap_set_security_param(ESP_BLE_SM_IOCAP_MODE, &iocap, sizeof(uint8_t)));
+    ESP_ERROR_CHECK(esp_ble_gap_set_security_param(ESP_BLE_SM_MAX_KEY_SIZE, &key_size, sizeof(uint8_t)));
+    ESP_ERROR_CHECK(esp_ble_gap_set_security_param(ESP_BLE_SM_SET_INIT_KEY, &init_key, sizeof(uint8_t)));
+    ESP_ERROR_CHECK(esp_ble_gap_set_security_param(ESP_BLE_SM_SET_RSP_KEY, &rsp_key, sizeof(uint8_t)));
+
+    return ESP_OK;
+}
+
